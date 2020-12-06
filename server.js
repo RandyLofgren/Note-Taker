81f03d620c3ac6019ee66ed4f54b931b90c20955
RandyLofgren <43276499+RandyLofgren@users.noreply.github.com>
2020-12-06 17:40:10 -0500

    got delete and post working

--- server.js
+++ server.js
@@ -13,56 +13,69 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'))
 
 
-app.get("/notes", function(req, res) {
+app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
-  });
+});
 
-  app.get("/api/notes", function(req, res) {
-    return res.json(characters);
-  });
+app.get("/api/notes", function (req, res) {
+    return res.json(dataBase);
+});
 
 
-  app.get("*", function(req, res) {
+app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
+});
 
 // Create New Characters - takes in JSON input
-app.post("/api/notes", function(req, res) {
+app.post("/api/notes", function (req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var notePage = req.body;
-  
-  
-  
+
+
+
     console.log(notePage);
-  
+
     dataBase.push(notePage);
+    dataBase.forEach((o, i) => o.id = i + 1);
+
     //Might need to change this to ./db/db.json
-     fs.writeFile(dataBase, JSON.stringify(dataBase), err => {
-         if (err){
-             console.log("You've got an error")
-             res.sendStatus(404);
-         } else {res.sendStatus(200);
-         }
-     }); 
+    fs.writeFile("./db/db.json", JSON.stringify(dataBase), err => {
+        if (err) {
+            console.log("You've got an error")
+            res.sendStatus(404);
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+
+app.delete("/api/notes/:id", function (req, res){
+
+    var deleted =  req.params.id;
+    for ( let i = 0; i < dataBase.length; i++){
+        if(dataBase[i].id == deleted){
+            dataBase.splice(i,1)
+            break;
+        }
+    }
+
+
+    fs.writeFile("./db/db.json", JSON.stringify(dataBase), err => {
+        if (err) {
+            console.log("You've got an error")
+            res.sendStatus(404);
+        } else {
+            res.sendStatus(200);
+        }
+    });
+})
 
 
 
-    res.json(newCharacter);
-  });
-  
-
-  
-
-
-
-
-
-
-
-  app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
-  
+});
